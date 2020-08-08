5a9ba6a883d62f58032168441d88860fec7a80c8
Rémy Dechelle <remy.dechelle@prevu3d.com>
2020-08-08 16:12:35 +0200

    Removed keys for public release

--- src/Utils/Constants.php
+++ src/Utils/Constants.php
@@ -6,11 +6,11 @@ namespace App\Utils;
 
 class Constants
 {
-    public const TMDB_API_KEY = "5cc5b11fa6bc7c0c39aa2b4807e8602b";
-    public const OMDB_API_KEY = "5144452c";
+    public const TMDB_API_KEY = "";
+    public const OMDB_API_KEY = "";
     public const DEFAULT_IMAGE_NAME = "default.jpg";
 
-    const SPOTIFY_CLIENT_ID = "d9623af040714e5bb4edf370ac44567e";
-    const SPOTIFY_CLIENT_SECRET = "2c3609b8b3e64628b3881d2baf4a7748";
+    const SPOTIFY_CLIENT_ID = "";
+    const SPOTIFY_CLIENT_SECRET = "";
     const SPOTIFY_REDIRECT_URI = "http://localhost:8000/callback";
 }
